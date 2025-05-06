260b208d722be42482173709a77e0191c2d67b90
Rijan Gautam <rijangautam07@gmail.com>
2025-05-06 22:21:14 +0545

    updated! Completed ORM

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.db.models import F, Func, Value, ExpressionWrapper, DecimalField, Q, Count
 from django.db.models.aggregates import Min, Max, Count, Avg, Sum
-from store.models import Product, Customer, Collection, Order, OrderItem
+from store.models import Product, Customer, Collection, Order, OrderItem, CartItem
 
 
 def say_hello(request):
-    count = OrderItem.objects.filter(order__id = 1).annotate(income = F('unit_price')*F('quantity')).aggregate(Sum('income'))
+    queryset = Order.objects.annotate(total_item = Sum('orderitem__quantity')).filter(total_item__gt = 6).select_related('customer')
+    count = queryset.count()
     return render(request, 'hello.html', {'name': 'Rijan',
+                                          'queryset': queryset,
                                           'count': count
                                           })
