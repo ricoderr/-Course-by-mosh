d103d395c45d4f36b5316c90de5ee27f7d8e7334
Rijan Gautam <rijangautam07@gmail.com>
2025-05-07 18:46:15 +0545

    finished learning basic admin customization (skipped some unnecessary boring lessons)

--- store/admin.py
+++ store/admin.py
@@ -3,5 +3,13 @@ from . import models
 admin.site.site_header="Rijan's authorized person space (-)"
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin): 
-    list_display = ['title', 'unit_price', 'collection']
+    list_display = ['title', 'unit_price', 'collection', 'inventory_status']
+    list_editable = ['collection']
+    ordering = ['title', 'unit_price']
+    list_per_page = 10
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product): 
+        if product.inventory < 10: 
+            return 'low'
+        return 'ok'
 admin.site.register(models.Collection)
